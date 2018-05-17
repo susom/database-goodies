152046573c8e55e3ddc0845d1f029b7c7eececb4
Garrick Olson <garricko@stanford.edu>
2018-05-17 01:09:25 -0700

    Add Avro support to Etl, along with tests (need more work on mssql and oracle) Upgrade to database 3.0-SNAPSHOT

--- src/main/java/com/github/susom/dbgoodies/etl/Etl.java
+++ src/main/java/com/github/susom/dbgoodies/etl/Etl.java
@@ -16,14 +16,30 @@
 package com.github.susom.dbgoodies.etl;
 
 import com.github.susom.database.Database;
+import com.github.susom.database.DatabaseException;
+import com.github.susom.database.Row;
 import com.github.susom.database.Schema;
 import com.github.susom.database.Sql;
 import com.github.susom.database.SqlArgs;
 import com.github.susom.database.SqlSelect;
+import java.io.File;
+import java.math.BigDecimal;
+import java.nio.ByteBuffer;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Types;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.function.Supplier;
 import javax.annotation.CheckReturnValue;
+import javax.annotation.Nonnull;
+import org.apache.avro.Schema.Field;
+import org.apache.avro.Schema.Type;
+import org.apache.avro.file.DataFileWriter;
+import org.apache.avro.generic.GenericData;
+import org.apache.avro.generic.GenericDatumWriter;
+import org.apache.avro.generic.GenericRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -54,6 +70,10 @@ public final class Etl {
       return new SaveAsTable(destination, tableName, select);
     }
 
+    @CheckReturnValue
+    public SaveAsAvro asAvro(String path, String schemaName, String tableName) {
+      return new SaveAsAvro(path, schemaName, tableName, select);
+    }
     // asCsv(), asTsv(), asExcel(), asJson(), asXml(), ...
   }
 
@@ -163,6 +183,264 @@ public final class Etl {
           alreadyLoggedTxWarning = true;
         }
       }
+    }
+  }
+
+  public static class SaveAsAvro {
+    private final String filename;
+    private final String schemaName;
+    private final String tableName;
+    private final SqlSelect select;
+    private int fetchSize = 100000;
+
+    SaveAsAvro(String filename, String schemaName, String tableName, SqlSelect select) {
+      this.filename = filename;
+      this.schemaName = schemaName;
+      this.tableName = tableName;
+      this.select = select;
+    }
+
+    /**
+     * Actually begin the database and file writing operations.
+     */
+    public void start() {
+      select.fetchSize(fetchSize).query(rs -> {
+        Builder builder = null;
+        DataFileWriter<GenericRecord> writer = null;
+
+        try {
+          while (rs.next()) {
+            if (builder == null) {
+              builder = new Builder(schemaName, tableName, rs);
+              writer = new DataFileWriter<GenericRecord>(new GenericDatumWriter<>(builder.schema()))
+//                  .setCodec(CodecFactory.nullCodec())
+                  .create(builder.schema(), new File(filename));
+              log.debug("Using schema: \n" + builder.schema().toString(true));
+            }
+
+            writer.append(builder.read(rs));
+          }
+        } finally {
+          if (writer != null) {
+            writer.close();
+          }
+        }
+
+        return null;
+      });
+    }
+
+    /**
+     * Allow control of the number of rows to be read at one time by the
+     * underlying JDBC driver (if supported). Default value is 100,000 rows.
+     */
+    @CheckReturnValue
+    SaveAsAvro fetchSize(int nbrRows) {
+      fetchSize = nbrRows;
+      return this;
+    }
+  }
+
+  private static class Builder {
+    private String[] names;
+    private final int[] types;
+    private final int[] precision;
+    private final int[] scale;
+    private org.apache.avro.Schema schema;
+    private String schemaName;
+    private String tableName;
+
+    Builder(String schemaName, String tableName, Row r) {
+      this.schemaName = schemaName;
+      this.tableName = tableName;
+      try {
+        ResultSetMetaData metadata = r.getMetadata();
+        int columnCount = metadata.getColumnCount();
+        names = new String[columnCount];
+        types = new int[columnCount];
+        precision = new int[columnCount];
+        scale = new int[columnCount];
+
+        for (int i = 0; i < columnCount; i++) {
+          names[i] = metadata.getColumnLabel(i + 1);
+          types[i] = metadata.getColumnType(i + 1);
+          precision[i] = metadata.getPrecision(i + 1);
+          scale[i] = metadata.getScale(i + 1);
+        }
+
+        names = SqlArgs.tidyColumnNames(names);
+      } catch (SQLException e) {
+        throw new DatabaseException("Unable to retrieve metadata from ResultSet", e);
+      }
+    }
+
+    org.apache.avro.Schema schema() {
+      if (schema == null) {
+        List<Field> fields = new ArrayList<>();
+
+        for (int i = 0; i < names.length; i++) {
+          switch (types[i]) {
+          case Types.SMALLINT:
+          case Types.INTEGER:
+            fields.add(new org.apache.avro.Schema.Field(names[i],
+                org.apache.avro.Schema.createUnion(org.apache.avro.Schema.create(Type.NULL), org.apache.avro.Schema.create(Type.INT)),
+                null, Field.NULL_VALUE));
+            break;
+          case Types.BIGINT:
+            fields.add(new org.apache.avro.Schema.Field(names[i],
+                org.apache.avro.Schema.createUnion(org.apache.avro.Schema.create(Type.NULL), org.apache.avro.Schema.create(Type.LONG)),
+                null, Field.NULL_VALUE));
+            break;
+          case Types.REAL:
+          case 100: // Oracle proprietary it seems
+            fields.add(new org.apache.avro.Schema.Field(names[i],
+                org.apache.avro.Schema.createUnion(org.apache.avro.Schema.create(Type.NULL), org.apache.avro.Schema.create(Type.FLOAT)),
+                null, Field.NULL_VALUE));
+            break;
+          case Types.DOUBLE:
+          case 101: // Oracle proprietary it seems
+            fields.add(new org.apache.avro.Schema.Field(names[i],
+                org.apache.avro.Schema.createUnion(org.apache.avro.Schema.create(Type.NULL), org.apache.avro.Schema.create(Type.DOUBLE)),
+                null, Field.NULL_VALUE));
+            break;
+          case Types.NUMERIC:
+            if (precision[i] == 10 && scale[i] == 0) {
+              // Oracle reports integer as numeric
+              fields.add(new org.apache.avro.Schema.Field(names[i],
+                  org.apache.avro.Schema.createUnion(org.apache.avro.Schema.create(Type.NULL), org.apache.avro.Schema.create(Type.INT)),
+                  null, Field.NULL_VALUE));
+            } else if (precision[i] == 19 && scale[i] == 0) {
+              // Oracle reports long as numeric
+              fields.add(new org.apache.avro.Schema.Field(names[i],
+                  org.apache.avro.Schema.createUnion(org.apache.avro.Schema.create(Type.NULL), org.apache.avro.Schema.create(Type.LONG)),
+                  null, Field.NULL_VALUE));
+            } else {
+              org.apache.avro.Schema bytes = org.apache.avro.Schema.create(Type.BYTES);
+              bytes.addProp("logical_type", "decimal");
+              bytes.addProp("precision", precision[i]);
+              bytes.addProp("scale", scale[i]);
+              fields.add(new org.apache.avro.Schema.Field(names[i],
+                  org.apache.avro.Schema.createUnion(org.apache.avro.Schema.create(Type.NULL), bytes),
+                  null, Field.NULL_VALUE));
+            }
+            break;
+          case Types.BINARY:
+          case Types.VARBINARY:
+          case Types.BLOB:
+            fields.add(new org.apache.avro.Schema.Field(names[i],
+                org.apache.avro.Schema.createUnion(org.apache.avro.Schema.create(Type.NULL), org.apache.avro.Schema.create(Type.BYTES)),
+                null, Field.NULL_VALUE));
+            break;
+          case Types.CLOB:
+          case Types.NCLOB:
+            fields.add(new org.apache.avro.Schema.Field(names[i],
+                org.apache.avro.Schema.createUnion(org.apache.avro.Schema.create(Type.NULL), org.apache.avro.Schema.create(Type.STRING)),
+                null, Field.NULL_VALUE));
+            break;
+          case Types.TIMESTAMP:
+            org.apache.avro.Schema date = org.apache.avro.Schema.create(Type.LONG);
+            date.addProp("logical_type", "timestamp_millis");
+            fields.add(new org.apache.avro.Schema.Field(names[i],
+                org.apache.avro.Schema.createUnion(org.apache.avro.Schema.create(Type.NULL), date),
+                null, Field.NULL_VALUE));
+            break;
+          case Types.NVARCHAR:
+          case Types.VARCHAR:
+          case Types.CHAR:
+          case Types.NCHAR:
+            if (precision[i] >= 2147483647) {
+              // Postgres seems to report clobs are varchar(2147483647)
+              fields.add(new org.apache.avro.Schema.Field(names[i],
+                  org.apache.avro.Schema.createUnion(org.apache.avro.Schema.create(Type.NULL), org.apache.avro.Schema.create(Type.STRING)),
+                  null, Field.NULL_VALUE));
+            } else {
+              fields.add(new org.apache.avro.Schema.Field(names[i],
+                  org.apache.avro.Schema.createUnion(org.apache.avro.Schema.create(Type.NULL), org.apache.avro.Schema.create(Type.STRING)),
+                  null, Field.NULL_VALUE));
+            }
+            break;
+          default:
+            throw new DatabaseException("Don't know how to deal with column type: " + types[i]);
+          }
+        }
+
+        schema = org.apache.avro.Schema.createRecord(tableName, null, schemaName, false, fields);
+      }
+      return schema;
+    }
+
+    @Nonnull
+    GenericRecord read(Row r) {
+      GenericRecord record = new GenericData.Record(schema());
+
+      for (int i = 0; i < names.length; i++) {
+        switch (types[i]) {
+        case Types.SMALLINT:
+        case Types.INTEGER:
+          record.put(names[i], r.getIntegerOrNull());
+          break;
+        case Types.BIGINT:
+          record.put(names[i], r.getLongOrNull());
+          break;
+        case Types.REAL:
+        case 100: // Oracle proprietary it seems
+          record.put(names[i], r.getFloatOrNull());
+          break;
+        case Types.DOUBLE:
+        case 101: // Oracle proprietary it seems
+          record.put(names[i], r.getDoubleOrNull());
+          break;
+        case Types.NUMERIC:
+          if (precision[i] == 10 && scale[i] == 0) {
+            // Oracle reports integer as numeric
+            record.put(names[i], r.getIntegerOrNull());
+          } else if (precision[i] == 19 && scale[i] == 0) {
+            // Oracle reports long as numeric
+            record.put(names[i], r.getLongOrNull());
+          } else {
+            BigDecimal bigDecimalOrNull = r.getBigDecimalOrNull();
+            if (bigDecimalOrNull == null) {
+              record.put(names[i], null);
+            } else {
+              if (bigDecimalOrNull.scale() != scale[i]) {
+                //noinspection BigDecimalMethodWithoutRoundingCalled - shouldn't happen, allow exception to propagate
+                bigDecimalOrNull = bigDecimalOrNull.setScale(scale[i]);
+              }
+              record.put(names[i], ByteBuffer.wrap(bigDecimalOrNull.unscaledValue().toByteArray()));
+            }
+          }
+          break;
+        case Types.BINARY:
+        case Types.VARBINARY:
+        case Types.BLOB:
+          byte[] bytesOrNull = r.getBlobBytesOrNull();
+          record.put(names[i], bytesOrNull == null ? null : ByteBuffer.wrap(bytesOrNull));
+          break;
+        case Types.CLOB:
+        case Types.NCLOB:
+          record.put(names[i], r.getClobStringOrNull());
+          break;
+        case Types.TIMESTAMP:
+          Date dateOrNull = r.getDateOrNull();
+          record.put(names[i], dateOrNull == null ? null : dateOrNull.getTime());
+          break;
+        case Types.NVARCHAR:
+        case Types.VARCHAR:
+        case Types.CHAR:
+        case Types.NCHAR:
+          if (precision[i] >= 2147483647) {
+            // Postgres seems to report clobs are varchar(2147483647)
+            record.put(names[i], r.getClobStringOrNull());
+          } else {
+            record.put(names[i], r.getStringOrNull());
+          }
+          break;
+        default:
+          throw new DatabaseException("Don't know how to deal with column type: " + types[i]);
+        }
+      }
+
+      return record;
     }
   }
 }
---
+++ src/test/java/com/github/susom/dbgoodies/test/EtlToAvroTest.java
@@ -0,0 +1,133 @@
+/*
+ * Copyright 2018 The Board of Trustees of The Leland Stanford Junior University.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.susom.dbgoodies.test;
+
+import com.github.susom.database.Database;
+import com.github.susom.database.DatabaseProvider;
+import com.github.susom.database.OptionsOverride;
+import com.github.susom.database.Schema;
+import com.github.susom.dbgoodies.etl.Etl;
+import java.math.BigDecimal;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Unit tests to verify saving query results to Avro works correctly. The tests
+ * are in this abstract base class, and get executed by subclasses named and
+ * configured for a specific vendor database.
+ */
+public abstract class EtlToAvroTest {
+  private DatabaseProvider dbp;
+  private Database db;
+  private Date now = new Date();
+
+  @Before
+  public void setupJdbc() {
+    dbp = createDatabaseProvider(new OptionsOverride() {
+      @Override
+      public Date currentDate() {
+        return now;
+      }
+
+      @Override
+      public Calendar calendarForTimestamps() {
+        return Calendar.getInstance(TimeZone.getTimeZone("America/Los_Angeles"));
+      }
+    });
+    db = dbp.get();
+    db.dropTableQuietly("dbtest");
+  }
+
+  abstract DatabaseProvider createDatabaseProvider(OptionsOverride options);
+
+  @After
+  public void closeJdbc() {
+    if (dbp != null) {
+      dbp.commitAndClose();
+    }
+  }
+
+  @Test
+  public void saveAllTypesAsAvro() {
+    new Schema().addTable("dbtest")
+        .addColumn("nbr_integer").asInteger().primaryKey().table()
+        .addColumn("nbr_long").asLong().table()
+        .addColumn("nbr_float").asFloat().table()
+        .addColumn("nbr_double").asDouble().table()
+        .addColumn("nbr_big_decimal").asBigDecimal(19, 9).table()
+        .addColumn("str_varchar").asString(80).table()
+        .addColumn("str_fixed").asStringFixed(1).table()
+        .addColumn("str_lob").asClob().table()
+        .addColumn("bin_blob").asBlob().table()
+        .addColumn("boolean_flag").asBoolean().table()
+        .addColumn("date_millis").asDate().schema().execute(db);
+
+    db.toInsert("insert into dbtest (nbr_integer, nbr_long, nbr_float, nbr_double, nbr_big_decimal, str_varchar,"
+        + " str_fixed, str_lob, bin_blob, boolean_flag, date_millis) values (?,?,?,?,?,?,?,?,?,?,?)")
+        .argInteger(Integer.MAX_VALUE).argLong(Long.MAX_VALUE).argDouble((double) Float.MAX_VALUE)
+        .argDouble(Double.MAX_VALUE).argBigDecimal(new BigDecimal("123.456"))
+        .argString("hello").argString("Z").argClobString("hello again")
+        .argBlobBytes(new byte[] { '1', '2' }).argBoolean(true).argDateNowPerApp().insert(1);
+
+    db.toInsert("insert into dbtest (nbr_integer, nbr_long, nbr_float, nbr_double, nbr_big_decimal, str_varchar,"
+        + " str_fixed, str_lob, bin_blob, boolean_flag, date_millis) values (?,?,?,?,?,?,?,?,?,?,?)")
+        .argInteger(Integer.MIN_VALUE).argLong(Long.MIN_VALUE).argDouble(0.000001d)
+        .argDouble(Double.MIN_VALUE).argBigDecimal(new BigDecimal("-123.456"))
+        .argString("goodbye").argString("A").argClobString("bye again")
+        .argBlobBytes(new byte[] { '3', '4' }).argBoolean(false).argDateNowPerApp().insert(1);
+
+    Etl.saveQuery(db.toSelect("select * from dbtest")).asAvro("target/dbtest.avro", null, "dbtest").start();
+  }
+
+  @Test
+  public void nullInteger() {
+    new Schema()
+        .addTable("dbtest")
+        .addColumn("pk").primaryKey().table()
+        .addColumn("nbr_integer").asInteger().schema().execute(db);
+
+    db.toInsert("insert into dbtest values (?,?)").argLong(1L).argInteger(null).insert(1);
+
+    Etl.saveQuery(db.toSelect("select * from dbtest")).asAvro("target/dbtest2.avro", null, "dbtest").start();
+  }
+
+  @Test
+  public void nullValues() {
+    new Schema()
+        .addTable("dbtest")
+        .addColumn("pk").primaryKey().table()
+        .addColumn("nbr_integer").asInteger().table()
+        .addColumn("nbr_long").asLong().table()
+        .addColumn("nbr_float").asFloat().table()
+        .addColumn("nbr_double").asDouble().table()
+        .addColumn("nbr_big_decimal").asBigDecimal(19, 9).table()
+        .addColumn("str_varchar").asString(80).table()
+        .addColumn("str_fixed").asStringFixed(1).table()
+        .addColumn("str_lob").asClob().table()
+        .addColumn("bin_blob").asBlob().table()
+        .addColumn("date_millis").asDate().table().schema().execute(db);
+
+    db.toInsert("insert into dbtest values (?,?,?,?,?,?,?,?,?,?,?)").argLong(1L).argInteger(null).argLong(null)
+        .argFloat(null).argDouble(null).argBigDecimal(null).argString(null).argString(null).argClobString(null)
+        .argBlobBytes(null).argDate(null).insert(1);
+
+    Etl.saveQuery(db.toSelect("select * from dbtest")).asAvro("target/dbtest3.avro", null, "dbtest").start();
+  }
+}
